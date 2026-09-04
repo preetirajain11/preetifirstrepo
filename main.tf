15cb393d029aa8126e04ba85a59cbe0fed951da2
preetirajain11 <preetirajain11@gmail.com>
2026-09-04 14:21:39 +0200

    new resource group

--- main.tf
+++ main.tf
@@ -2,3 +2,6 @@ resource "azurerm_resource_group" "tf" {
   name     = "repo-rg"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "ab" {
+  name     = "repo-rg2"
+  location = "West Europe"}
